4c339e80b7f5c6f09e7faf7235d96b61dcede5f8
Cyril.Bazyliev <kirill.realcom@gmail.com>
2018-06-25 15:56:41 +0300

    ready desktop and adaptive

--- app/js/common.js
+++ app/js/common.js
@@ -4,7 +4,15 @@ $(document).ready(function() {
 		nav: true,
 		dots: false,
 		loop: true,
-		navText: ['<i class="icon-keyboard_arrow_left"></i>', '<i class="icon-keyboard_arrow_right"></i>']
+		navText: ['<i class="icon-keyboard_arrow_left"></i>', '<i class="icon-keyboard_arrow_right"></i>'],
+		responsive: {
+			0: {
+				items: 1
+			},
+			992: {
+				items: 2
+			},
+		}
 	});
 	$(".start_scroll").on("click","a", function (event) {
     event.preventDefault();
@@ -17,5 +25,9 @@ $(document).ready(function() {
     var id  = $(this).attr('href'),
 		top = $(id).offset().top;
 		$('body,html').animate({scrollTop: top}, 1000);
+  });
+  $('.header_button-xs .__btn').click(function() {
+  	$($(this)).toggleClass('open')
+  	$('.header_nav').toggleClass('open')
   });
 });
